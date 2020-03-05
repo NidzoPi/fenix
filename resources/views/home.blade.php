e9398693b9e362ed1c1f3ca5e385589c54be53ba
NidzoPi <pilipovicnikola68@gmail.com>
2020-03-05 15:11:01 +0100

    Laravel installed and Username added front and back

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-9" style="padding-left: 150px;">
+
+                <div> <h1> Fenix </h1></div>
+                <div class="d-flex">
+                    <div class="pr-3"><strong> Broj članova: 27 </strong></div>
+                    <div class="pr-3"><strong> Broj akcija: 10 </strong></div>
+                    <div class="pr-3"><strong> Datum učlanjenja: 5.3.2020. </strong></div>
+                </div>
+
+                <div class="pt-4"> KOSTAJNICA  </div>
+                <div> About section </div>
+                <div> <a href="https://www.facebook.com/okfenix"> Facebook </a> </div>
+        </div>
+        <div class="col-3">
+
+                <img src="https://scontent.fbeg4-1.fna.fbcdn.net/v/t1.0-9/71209701_119529512772788_5338810469673074688_n.png?_nc_cat=109&_nc_sid=85a577&_nc_ohc=ktNzTeSfUoEAX9VKpIa&_nc_ht=scontent.fbeg4-1.fna&oh=7c929728b9a86025a90d699c68c13597&oe=5E94F2FB" class="img-fluid rounded-circle" height="300">
+
+        </div>
+
+        <div class="row pt-5">
+
+            <div class="col-4"><img src = "https://scontent.fbeg4-1.fna.fbcdn.net/v/t1.0-9/81791209_176065987119140_9153136024346427392_n.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=Z9QpwqUIWcQAX8cTQQr&_nc_ht=scontent.fbeg4-1.fna&oh=ef0db005dd7f7766f7d4fa6a24b042c3&oe=5E9A2E42" class="w-100"></div>
+            <div class="col-4"><img src = "https://scontent.fbeg4-1.fna.fbcdn.net/v/t1.0-9/81791209_176065987119140_9153136024346427392_n.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=Z9QpwqUIWcQAX8cTQQr&_nc_ht=scontent.fbeg4-1.fna&oh=ef0db005dd7f7766f7d4fa6a24b042c3&oe=5E9A2E42" class="w-100"></div>
+            <div class="col-4"><img src = "https://scontent.fbeg4-1.fna.fbcdn.net/v/t1.0-9/81791209_176065987119140_9153136024346427392_n.jpg?_nc_cat=110&_nc_sid=110474&_nc_ohc=Z9QpwqUIWcQAX8cTQQr&_nc_ht=scontent.fbeg4-1.fna&oh=ef0db005dd7f7766f7d4fa6a24b042c3&oe=5E9A2E42" class="w-100"></div>
+
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                    <div style="border-right:1px solid #333; padding-right: 10px">Volonteri Srpske</div>
+                    <div><img src="https://scontent.fbeg4-1.fna.fbcdn.net/v/t1.0-9/10537869_1451675448304571_4203704378045042734_n.jpg?_nc_cat=101&_nc_sid=85a577&_nc_ohc=sYO9NaC6s6wAX9NH4m_&_nc_ht=scontent.fbeg4-1.fna&oh=cec5749e92f33da0ce057666d3ab3475&oe=5E80EDC5" style="height: 35px; padding-left: 15px"></script></div>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
